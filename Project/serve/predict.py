0a6f8278dc981738345aad5af2d547bb48927386
EC2 Default User <ec2-user@ip-172-16-41-53.us-west-2.compute.internal>
2020-08-04 06:28:47 +0000

    Update predict script

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -58,20 +58,30 @@ def output_fn(prediction_output, accept):
     return str(prediction_output)
 
 def predict_fn(input_data, model):
-    print('Inferring sentiment of input data.')
+    print('Inferring sentiment of input data. Data=' + input_data)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
+
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
+
     # TODO: Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
+    print('Word_dict')
+    print(model.word_dict)
     data_X = None
     data_len = None
+    
+    preprocessedInputData = review_to_words(input_data)
+    encodedData, theLength = convert_and_pad(model.word_dict, [preprocessedInputData])
+
+    data_X = pd.concat([pd.DataFrame(theLength), pd.DataFrame(encodedData)], axis=1)
+
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,9 +94,17 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
+    score = predictor.predict(data)
+    print(score)
+    
+    result = None
+    if score[0] > 0.5:
+        result = np.array([1])
+    else:
+        result = np.array([0])
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+
 
     return result
